bcfc8e27877c2692b847136d95fa21abf38fb3a6
Marius <miclean.marius88@gmail.com>
2022-04-16 22:03:32 +0300

    Login and Register brut

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,7 +10,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     /**
-     * @Route("/dashboard", name="app_dashboard")
+     * @Route("/", name="app_main")
+     */
+    public function main(): Response
+    {
+        if ($this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->redirectToRoute('dashboard');
+    }
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     * @Security("is_granted('ROLE_ADMIN')", statusCode=404)
      */
     public function index(): Response
     {
